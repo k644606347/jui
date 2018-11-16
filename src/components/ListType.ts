2548c874d06468d4b08e6f8721c41a47447299f2
刘江宇 <jiangyu3@staff.sina.com.cn>
2018-11-16 17:23:07 +0800

    ...

--- src/components/ListType.ts
+++ src/components/ListType.ts
@@ -14,11 +14,11 @@ export interface RowData {
   [k: string]: any;
 }
 export interface IListProps {
-  dataSource: Array<SectionData | RowData | never>;
+  dataSource: Array<SectionData | RowData | any>;
   style?: React.CSSProperties;
   className?: string;
   sectionHeaderSticky?: boolean;
-  renderRow: (data: RowData) => JSX.Element;
+  renderRow: (data: RowData | any) => JSX.Element;
   renderSeparator?: (data: RowData) => JSX.Element;
   // renderSectionWrapper?: (header: JSX.Element, body: JSX.Element) => JSX.Element;
   renderSectionHeader?: (data: SectionData) => JSX.Element;
