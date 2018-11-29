ddb24cacc457a4d257897563e0914f305f5fb2c8
刘江宇 <jiangyu3@staff.sina.com.cn>
2018-11-29 13:53:36 +0800

    ...

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -14,9 +14,6 @@ module.exports = {
     output: {
         // Add /* filename */ comments to generated require()s in the output.
         pathinfo: true,
-        // This does not produce a real file. It's just the virtual path that is served
-        // by WebpackDevServer in development. This is the JS bundle containing code
-        // from all our entry points, and the Webpack runtime.
         filename: '[name].js',
         chunkFilename: '[name].chunk.js',
     },
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -19,9 +19,6 @@ module.exports = {
         path: constants.appBuild,
         // Add /* filename */ comments to generated require()s in the output.
         pathinfo: true,
-        // This does not produce a real file. It's just the virtual path that is served
-        // by WebpackDevServer in development. This is the JS bundle containing code
-        // from all our entry points, and the Webpack runtime.
         filename: '[name].js',
         chunkFilename: '[name].chunk.js',
     },
