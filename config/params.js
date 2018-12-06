4e823de3aa455f657e0370138351bc7ff3ff88b2
jiangyu3 <k644606347@126.com>
2018-12-06 16:40:12 +0800

    ...

--- config/params.js
+++ config/params.js
@@ -5,6 +5,8 @@ module.exports = {
     classNamePrefix: 'jui',
     demoIndex: path.resolve('./src/demo/index.tsx'),
     appIndex: path.resolve('./src/index.tsx'),
+    srcPath: path.resolve('./src'),
+    packageJSON: path.resolve('./package.json'),
     appBuild: path.resolve('../jui-release/lib'),
     nodeModulesPath: path.resolve('./node_modules'),
 };
