70e6eece3b1a79a06366861c8ea85729d6568d0a
jiangyu3 <k644606347@126.com>
2018-12-27 13:10:24 +0800

    1.重构ActiveForm，减少rerender次数，提高性能，增强校验逻辑，增强错误提示及容错能力 2.Widget通过额外嵌套<Field>来实现关联ActiveForm，Widget内部不再自动关联ActiveForm 3. Widget增加validate静态方法，会在ActiveForm校验时调用 4.去除Widget无用的代码

--- src/components/formWidget/ActiveFormContext.ts
+++ src/components/formWidget/ActiveFormContext.ts
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { Report } from "./Validator";
 import Widget, { FormWidgetProps } from "./Widget";
+import { FieldChangeEvent } from "./Field";
 type ReportMap = {[k in string]: Report};
+
+// React.Component<FormWidgetProps> & Widget
 export interface ActiveFormContextType {
     validating: boolean,
     isValid: boolean,
     validateReportMap: ReportMap,
     submitting: boolean,
-    onWidgetMount?: (widgetInstance: React.Component<FormWidgetProps> & Widget) => void,
+    onFieldMount(instace: React.ReactInstance): void,
+    onFieldChange(e: FieldChangeEvent): void,
 }
 export const ActiveFormContext = React.createContext({} as ActiveFormContextType);
